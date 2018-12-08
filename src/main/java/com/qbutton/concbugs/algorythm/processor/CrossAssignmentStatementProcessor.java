79183cb16a3062ccbe4a0dd4561a9029b651ac09
herman.urikh <herman.urikh@aengel.ru>
2018-12-08 13:51:46 +0300

    Concbugs: CrossAssignmentStatementProcessor implemented.

--- src/main/java/com/qbutton/concbugs/algorythm/processor/CrossAssignmentStatementProcessor.java
+++ src/main/java/com/qbutton/concbugs/algorythm/processor/CrossAssignmentStatementProcessor.java
@@ -1,13 +1,38 @@
 package com.qbutton.concbugs.algorythm.processor;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.qbutton.concbugs.algorythm.dto.EnvEntry;
+import com.qbutton.concbugs.algorythm.dto.HeapObject;
 import com.qbutton.concbugs.algorythm.dto.State;
 import com.qbutton.concbugs.algorythm.dto.statement.CrossAssignmentStatement;
+import com.qbutton.concbugs.algorythm.exception.AlgorithmValidationException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 final class CrossAssignmentStatementProcessor extends AbstractStatementProcessor<CrossAssignmentStatement> {
 
     @Override
     State process(CrossAssignmentStatement statement, State originalState) {
-        //TODO
-        return null;
+
+        String originalVar = statement.getRightValueName();
+
+        HeapObject existingHeapObject = originalState.getEnvironment().stream()
+                .filter(envEntry -> originalVar.equals(envEntry.getVarName()))
+                .findAny()
+                .map(EnvEntry::getHeapObject)
+                .orElseThrow(() -> new AlgorithmValidationException("no envEntry found for varName " + originalVar));
+
+        List<EnvEntry> newEnv = new ArrayList<>(originalState.getEnvironment());
+        newEnv.add(new EnvEntry(statement.getVarName(), existingHeapObject));
+
+        return new State(
+                originalState.getGraph().clone(),
+                ImmutableSet.copyOf(originalState.getRoots()),
+                ImmutableList.copyOf(originalState.getLocks()),
+                newEnv,
+                ImmutableSet.copyOf(originalState.getWaits())
+        );
     }
 }
---
+++ src/test/java/com/qbutton/concbugs/algorythm/processor/CrossAssignmentStatementProcessorTest.java
@@ -0,0 +1,89 @@
+package com.qbutton.concbugs.algorythm.processor;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.qbutton.concbugs.algorythm.dto.EnvEntry;
+import com.qbutton.concbugs.algorythm.dto.Graph;
+import com.qbutton.concbugs.algorythm.dto.HeapObject;
+import com.qbutton.concbugs.algorythm.dto.ProgramPoint;
+import com.qbutton.concbugs.algorythm.dto.State;
+import com.qbutton.concbugs.algorythm.dto.statement.CrossAssignmentStatement;
+import com.qbutton.concbugs.algorythm.exception.AlgorithmValidationException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Set;
+
+import static com.google.common.collect.ImmutableMap.of;
+import static java.util.Collections.emptySet;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@DisplayName("CrossAssignmentStatementProcessor")
+class CrossAssignmentStatementProcessorTest {
+
+    @Test
+    @DisplayName("processes correctly when there is correct data in original state")
+    void process_success() {
+        //given
+        CrossAssignmentStatementProcessor processor = new CrossAssignmentStatementProcessor();
+        String varName = "v1";
+        String rightVarName = "v2";
+        CrossAssignmentStatement statement = new CrossAssignmentStatement(32, varName, rightVarName);
+
+        HeapObject ho1 = new HeapObject(new ProgramPoint(rightVarName, 2), String.class);
+        Set<HeapObject> ho1Set = ImmutableSet.of(ho1);
+        List<HeapObject> ho1List = ImmutableList.of(ho1);
+        Graph ho1Graph = new Graph(of(ho1, emptySet()));
+        EnvEntry ho1EnvEntry = new EnvEntry(rightVarName, ho1);
+        State originalState = new State(
+                ho1Graph,
+                ho1Set,
+                ho1List,
+                ImmutableList.of(ho1EnvEntry),
+                ho1Set
+        );
+
+        //when
+        State newState = processor.process(statement, originalState);
+
+        //then
+        assertThat(newState.getGraph(), is(ho1Graph));
+        assertThat(newState.getRoots(), is(ho1Set));
+        assertThat(newState.getLocks(), is(ho1List));
+        assertThat(newState.getWaits(), is(ho1Set));
+        List<EnvEntry> newEnv = newState.getEnvironment();
+        assertThat(newEnv.size(), is(2));
+        assertThat(newEnv.get(0), is(ho1EnvEntry));
+        assertThat(newEnv.get(1), is(new EnvEntry(varName, ho1)));
+    }
+
+    @Test
+    @DisplayName("fails with exception when there is no env entry for given rightVarName in original state")
+    void process_failure_stateIsIncorrect() {
+        //given
+        CrossAssignmentStatementProcessor processor = new CrossAssignmentStatementProcessor();
+        String varName = "v1";
+        String rightVarName = "v2";
+        CrossAssignmentStatement statement = new CrossAssignmentStatement(32, varName, rightVarName);
+
+        HeapObject ho1 = new HeapObject(new ProgramPoint(rightVarName, 2), String.class);
+        Set<HeapObject> ho1Set = ImmutableSet.of(ho1);
+        List<HeapObject> ho1List = ImmutableList.of(ho1);
+        Graph ho1Graph = new Graph(of(ho1, emptySet()));
+        EnvEntry ho1EnvEntry = new EnvEntry("vv2", ho1);
+        State originalState = new State(
+                ho1Graph,
+                ho1Set,
+                ho1List,
+                ImmutableList.of(ho1EnvEntry),
+                ho1Set
+        );
+
+        //when
+        //then
+        assertThrows(AlgorithmValidationException.class, () -> processor.process(statement, originalState));
+    }
+}
