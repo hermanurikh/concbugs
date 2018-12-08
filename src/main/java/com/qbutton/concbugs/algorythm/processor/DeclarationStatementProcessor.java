8bac7a09185a2a18c1858f29747b19a72ccaeea0
herman.urikh <herman.urikh@aengel.ru>
2018-12-08 13:24:04 +0300

    Concbugs: DeclarationStatementProcessor implemented.

--- src/main/java/com/qbutton/concbugs/algorythm/processor/DeclarationStatementProcessor.java
+++ src/main/java/com/qbutton/concbugs/algorythm/processor/DeclarationStatementProcessor.java
@@ -1,13 +1,37 @@
 package com.qbutton.concbugs.algorythm.processor;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.qbutton.concbugs.algorythm.dto.EnvEntry;
+import com.qbutton.concbugs.algorythm.dto.HeapObject;
+import com.qbutton.concbugs.algorythm.dto.ProgramPoint;
 import com.qbutton.concbugs.algorythm.dto.State;
 import com.qbutton.concbugs.algorythm.dto.statement.DeclarationStatement;
+
+import java.util.ArrayList;
+import java.util.List;
 
 final class DeclarationStatementProcessor extends AbstractStatementProcessor<DeclarationStatement> {
 
     @Override
     State process(DeclarationStatement statement, State originalState) {
-        //TODO
-        return null;
+
+        EnvEntry newEnvEntry = new EnvEntry(
+                statement.getVarName(),
+                new HeapObject(
+                        new ProgramPoint(statement.getVarName(), statement.getLineNumber()),
+                        statement.getClazz()
+                ));
+
+        List<EnvEntry> newEnv = new ArrayList<>(originalState.getEnvironment());
+        newEnv.add(newEnvEntry);
+
+        return new State(
+                originalState.getGraph().clone(),
+                ImmutableSet.copyOf(originalState.getRoots()),
+                ImmutableList.copyOf(originalState.getLocks()),
+                newEnv,
+                ImmutableSet.copyOf(originalState.getWaits())
+        );
     }
 }
