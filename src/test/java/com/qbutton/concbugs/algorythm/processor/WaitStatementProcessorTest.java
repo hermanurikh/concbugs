2c71417e269efadd83df6943d26b3b6798e385e7
hermanurikh-laptop <herman.urikh@aengel.ru>
2018-12-08 22:18:08 +0300

    Concbugs: WaitStatementProcessorTest finished.

--- src/test/java/com/qbutton/concbugs/algorythm/processor/WaitStatementProcessorTest.java
+++ src/test/java/com/qbutton/concbugs/algorythm/processor/WaitStatementProcessorTest.java
@@ -9,6 +9,7 @@ import com.qbutton.concbugs.algorythm.dto.HeapObject;
 import com.qbutton.concbugs.algorythm.dto.ProgramPoint;
 import com.qbutton.concbugs.algorythm.dto.State;
 import com.qbutton.concbugs.algorythm.dto.statement.WaitStatement;
+import com.qbutton.concbugs.algorythm.exception.AlgorithmValidationException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -60,24 +61,122 @@ class WaitStatementProcessorTest {
     @Test
     @DisplayName("processes correctly when original locks are not empty and last lock is different from wait object")
     void process_success_locksAreNotEmptyAndTailDiffers() {
-        fail();
+        //given
+        WaitStatementProcessor processor = new WaitStatementProcessor();
+        String waitVarName = "campaignId";
+        HeapObject waitHeapObject = new HeapObject(new ProgramPoint(waitVarName, 23), Long.class);
+        HeapObject heapObject = new HeapObject(new ProgramPoint("clientId", 25), Integer.class);
+        List<EnvEntry> originalEnv = ImmutableList.of(new EnvEntry(waitVarName, waitHeapObject));
+
+        WaitStatement waitStatement = new WaitStatement(35, waitVarName);
+        Graph originalGraph = new Graph(ImmutableMap.of(heapObject, Collections.emptySet()));
+        Set<HeapObject> originalRoots = ImmutableSet.of(heapObject);
+        Set<HeapObject> originalWaits = ImmutableSet.of(heapObject);
+        List<HeapObject> originalLocks = ImmutableList.of(new HeapObject(new ProgramPoint("some name", 33), String.class), heapObject);
+        State originalState = new State(
+                originalGraph,
+                originalRoots,
+                originalLocks,
+                originalEnv,
+                originalWaits);
+        //when
+        State newState = processor.process(waitStatement, originalState);
+
+        //then
+        Map<HeapObject, Set<HeapObject>> newGraph = newState.getGraph().getNeighbors();
+        assertThat(newGraph.size(), is(2));
+        assertTrue(newGraph.containsKey(waitHeapObject));
+        assertTrue(newGraph.containsKey(heapObject));
+        assertThat(newGraph.get(heapObject).size(), is(1));
+        assertTrue(newGraph.get(heapObject).contains(waitHeapObject));
+
+        assertThat(newState.getRoots(), is(originalRoots));
+        assertThat(newState.getLocks(), is(originalLocks));
+        assertThat(newState.getEnvironment(), is(originalEnv));
+        assertThat(newState.getWaits(), is(originalWaits));
     }
 
     @Test
     @DisplayName("processes correctly when original locks are not empty and last lock is same as wait object")
     void process_success_locksAreNotEmptyAndTailIsSame() {
-        fail();
+        //given
+        WaitStatementProcessor processor = new WaitStatementProcessor();
+        String waitVarName = "campaignId";
+        HeapObject waitHeapObject = new HeapObject(new ProgramPoint(waitVarName, 23), Long.class);
+        HeapObject heapObject = new HeapObject(new ProgramPoint("clientId", 25), Integer.class);
+        List<EnvEntry> originalEnv = ImmutableList.of(new EnvEntry(waitVarName, waitHeapObject));
+
+        WaitStatement waitStatement = new WaitStatement(35, waitVarName);
+        Graph originalGraph = new Graph(ImmutableMap.of(heapObject, Collections.emptySet()));
+        Set<HeapObject> originalRoots = ImmutableSet.of(heapObject);
+        Set<HeapObject> originalWaits = ImmutableSet.of(heapObject);
+        List<HeapObject> originalLocks = ImmutableList.of(new HeapObject(new ProgramPoint("some name", 33), String.class), waitHeapObject);
+        State originalState = new State(
+                originalGraph,
+                originalRoots,
+                originalLocks,
+                originalEnv,
+                originalWaits);
+        //when
+        State newState = processor.process(waitStatement, originalState);
+
+        //then
+        assertThat(newState.getGraph(), is(originalGraph));
+        assertThat(newState.getRoots(), is(originalRoots));
+        assertThat(newState.getLocks(), is(originalLocks));
+        assertThat(newState.getEnvironment(), is(originalEnv));
+        assertThat(newState.getWaits(), is(originalWaits));
     }
 
     @Test
     @DisplayName("throws an exception when env does not contain mapping for wait variable")
     void process_fail_envIsIncorrect() {
-        fail();
+        //given
+        WaitStatementProcessor processor = new WaitStatementProcessor();
+        String waitVarName = "campaignId";
+        HeapObject waitHeapObject = new HeapObject(new ProgramPoint(waitVarName, 23), Long.class);
+        HeapObject heapObject = new HeapObject(new ProgramPoint("clientId", 25), Integer.class);
+        List<EnvEntry> originalEnv = ImmutableList.of(new EnvEntry("otherVarName", waitHeapObject));
+
+        WaitStatement waitStatement = new WaitStatement(35, waitVarName);
+        Graph originalGraph = new Graph(ImmutableMap.of(heapObject, Collections.emptySet()));
+        Set<HeapObject> originalRoots = ImmutableSet.of(heapObject);
+        Set<HeapObject> originalWaits = ImmutableSet.of(heapObject);
+        List<HeapObject> originalLocks = Collections.emptyList();
+        State originalState = new State(
+                originalGraph,
+                originalRoots,
+                originalLocks,
+                originalEnv,
+                originalWaits);
+        //when
+        //then
+        assertThrows(AlgorithmValidationException.class, () -> processor.process(waitStatement, originalState));
     }
 
     @Test
     @DisplayName("throws an exception when graph does not contain lock")
     void process_fail_graphIsIncorrect() {
-        fail();
+        //given
+        WaitStatementProcessor processor = new WaitStatementProcessor();
+        String waitVarName = "campaignId";
+        HeapObject waitHeapObject = new HeapObject(new ProgramPoint(waitVarName, 23), Long.class);
+        HeapObject heapObject = new HeapObject(new ProgramPoint("clientId", 25), Integer.class);
+        List<EnvEntry> originalEnv = ImmutableList.of(new EnvEntry(waitVarName, waitHeapObject));
+
+        WaitStatement waitStatement = new WaitStatement(35, waitVarName);
+        Graph originalGraph = new Graph(ImmutableMap.of(waitHeapObject, Collections.emptySet()));
+        Set<HeapObject> originalRoots = ImmutableSet.of(heapObject);
+        Set<HeapObject> originalWaits = ImmutableSet.of(heapObject);
+        List<HeapObject> originalLocks = ImmutableList.of(new HeapObject(new ProgramPoint("some name", 33), String.class), heapObject);
+        State originalState = new State(
+                originalGraph,
+                originalRoots,
+                originalLocks,
+                originalEnv,
+                originalWaits);
+        //when
+        //then
+        assertThrows(AlgorithmValidationException.class, () -> processor.process(waitStatement, originalState));
     }
 }
